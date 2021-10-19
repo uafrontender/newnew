880637f96b66149d46c762ee9ed9486754e7f660
uafrontender <mikhail.karkachov@gmail.com>
2021-10-19 14:45:45 +0300

    feat: changed globalStyles to tsx extension to use the Redux selectors for color mode, added temp examples

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,33 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
+import Link from 'next/link'
+import { toggleColorModeWithLS, _setColorMode } from '../redux-store/slices/uiStateSlice';
+import { useAppDispatch, useAppSelector } from '../redux-store/store';
 
 const Home: NextPage = () => {
+  const dispatch = useAppDispatch();
+  const { colorMode } = useAppSelector(state => state.ui)
+
   return (
     <div>
       <main>
         <h1>
           Welcome to NewNew!
         </h1>
+        <button
+          onClick={() => dispatch(_setColorMode(colorMode === 'dark' ? 'light' : 'dark'))}
+        >
+          Toggle dark mode
+        </button>
+        <button
+          onClick={() => dispatch(toggleColorModeWithLS())}
+        >
+          Toggle dark mode using thunk with LS
+        </button>
+        <div>
+          <Link href="/test">
+            <a>Link to test page</a>
+          </Link>
+        </div>
       </main>
     </div>
   )
