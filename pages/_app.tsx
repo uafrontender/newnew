eef4ad471121178231c75fec7afdf4618c539379
uafrontender <mikhail.karkachov@gmail.com>
2021-10-19 14:44:52 +0300

    feat: added typed Redux store and UI slice with colorMode, wrapped the _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import type { AppProps } from 'next/app'
+
+// Global CSS configurations
 import GlobalStyle from "../styles/globalStyles";
+
+// Redux store and provider
+import store from '../redux-store/store'
+import { Provider } from 'react-redux'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <GlobalStyle darkMode />
+    <Provider store={store}>
+      <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 export default MyApp
--- redux-store/store.ts
+++ redux-store/store.ts
@@ -1,3 +1,24 @@
-// TODO: initialize Redux store
+import { configureStore, combineReducers, ThunkAction, Action } from '@reduxjs/toolkit'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
-export default {}
+// Import reducers
+import uiReducer from './slices/uiStateSlice'
+
+const rootReducer = combineReducers({
+  ui: uiReducer,
+})
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NEXT_PUBLIC_NODE_ENV !== 'production',
+})
+
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>
+
+// Used in application instead of regular `useDispatch` and `useSelector` to support correct typings
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+export default store
