b57d77b61893f03ec4a0cf046c5c0ca5f5af971d
Andrii Uhryn <ugrin14081994@gmail.com>
2021-10-19 12:25:11 +0300

    Initial config

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from 'next/app'
+import GlobalStyle from "../styles/globalStyles";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle darkMode />
+      <Component {...pageProps} />
+    </>
+  );
+}
+export default MyApp
---
+++ redux-store/store.ts
@@ -0,0 +1,3 @@
+// TODO: initialize Redux store
+
+export default {}
