79191ea0554b580a0c2aeba6d260bdf2432894c9
uafrontender <mikhail.karkachov@gmail.com>
2021-10-19 12:25:11 +0300

    Initial config

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <main>
+        <h1>
+          Welcome to NewNew!
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
---
+++ styles/globalStyles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+
+// TODO: Implement CSS resets, main colors and fonts in GlobalStyles
+interface GlobalStylesInterface {
+    darkMode: boolean
+}
+
+const GlobalStyle = createGlobalStyle<GlobalStylesInterface>`
+  body {
+    color: ${props => (props.darkMode ? 'white' : 'black')};
+    background-color: ${props => (props.darkMode ? 'black' : 'white')};
+  }
+`
+
+export default GlobalStyle;
