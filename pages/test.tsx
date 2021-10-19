0bd2ccc0004caa9df91e0770f7b46b1368d233ec
Dmitry <dmitry@newnew.co>
2021-10-19 14:45:45 +0300

    feat: changed globalStyles to tsx extension to use the Redux selectors for color mode, added temp examples

---
+++ pages/test.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+
+const Test: NextPage = () => {
+
+  return (
+    <div>
+      <main>
+        <h1>
+          I am a test page
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Test
