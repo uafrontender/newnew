00aa946c6a33b9fdd663715fed6e3e26373e9b16
uafrontender <mikhail.karkachov@gmail.com>
2021-10-26 10:58:39 +0300

    Squashed commit of the following:
    commit 12f429211215422c6c659be5ffeb61cc74e324be
    Author: Dmitry <dmitry@newnew.co>
    Date:   Tue Oct 26 10:10:45 2021 +0300
    
        Comment out socket context use
    
        Commented out the usage of SocketContextProvider
        in _app.tsx, and restored the initial code in
        /pages/test.tsx while don't have a live endpoint
        to connect to.
    
    commit b0ec1a84fbbcdc841c537e9c32914f5205d8f9b3
    Author: Dmitry <dmitry@newnew.co>
    Date:   Tue Oct 26 09:53:46 2021 +0300
    
        Move socketContext to /contexts
    
    commit 9ca94204ce155de38395e59d20e6df85e6d3c927
    Author: Dmitry <dmitry@newnew.co>
    Date:   Mon Oct 25 17:27:26 2021 +0300
    
        Update example page
    
    commit f1626eca315c2e151b1e04bda6f2b514d9b64aaf
    Author: Dmitry <dmitry@newnew.co>
    Date:   Mon Oct 25 17:27:03 2021 +0300
    
        Implement socket context
    
    commit f36d1ddd49ae97893ee1ea6745bf4e60389b704b
    Author: Dmitry <dmitry@newnew.co>
    Date:   Mon Oct 25 17:25:24 2021 +0300
    
        Add socket.io-client as a dependency
    
    commit 0af813c512ac346acf63e05c67d0b656d457ef36
    Author: Dmitry <dmitry@newnew.co>
    Date:   Mon Oct 25 17:24:34 2021 +0300
    
        Update Eslint ignore prop-types
    
    commit e15bcb04c516e4219c41e297ba849874eed7b5b4
    Author: Dmitry <dmitry@newnew.co>
    Date:   Mon Oct 25 14:06:36 2021 +0300
    
        Define generic wrapper for API calls
    
        Defined a generic wrapper around browser Fetch API
        that can be used to construct functions for concrete
        requests based on it. Defined a couple of functions in
        endpoints folder, provided rough examples for on-page
        and SSR calls in the test.tsx file. Deleted pages/api/hello.ts
        to tidy up the structure.
    
    commit 8155eb4ae8d48b2d70422f99ca46f32dd598ebec
    Author: Dmitry <dmitry@newnew.co>
    Date:   Mon Oct 25 11:48:09 2021 +0300
    
        Add protobufjs to dependecies
    
        Protobufjs is required in newnew-api package,
        so it is downloaded already, and its typings
        are also needed in our code to define helper
        methods and interfaces, that is why added it
        as a regular dependency.
    
    commit 99319e514d82d53a7956cbfe11f21d79b5806fe3
    Author: Dmitry <dmitry@newnew.co>
    Date:   Mon Oct 25 11:08:33 2021 +0300
    
        Update Eslint better TS tooling
    
        Updated Eslint rules to to have adequate rules
        for no-unused-vars, as the previous version
        was raising errors when defining TS interfaces.
    
    commit a288489abd647437a2eead31d0252c43737d5c02
    Author: Dmitry <dmitry@newnew.co>
    Date:   Fri Oct 22 12:25:32 2021 +0300
    
        Define generic API-connect functions
    
        Defined initial versions for generic functions
        that that will make requests to the API
    
    commit 08d02f83c0e8a36280d501ac554f93ded25a2b97
    Author: Dmitry <dmitry@newnew.co>
    Date:   Thu Oct 21 18:21:44 2021 +0300
    
        Start defining API calling methods

---
+++ api/endpoints/auth.ts
@@ -0,0 +1,72 @@
+import { newnewapi } from 'newnew-api';
+import {
+  BASE_URL,
+  fetchProtobuf,
+} from '../apiConfigs';
+
+const BASE_URL_AUTH = `${BASE_URL}/auth`;
+
+// Email sign up
+/*
+These functions will be called to sign up a user with an email, following this logic:
+sendVerificationEmail(email) -> redirect to verification code page -> sendSignInRequest(email, code)
+*/
+
+/**
+ * Called from the Sign up page.
+ * If the request is successful, the input email is cached, and
+ * user is redirected to Code Verification page.
+*/
+export const sendVerificationEmail = (
+  payload: newnewapi.SendVerificationEmailRequest,
+) => fetchProtobuf<newnewapi.SendVerificationEmailRequest, newnewapi.EmptyResponse>(
+  newnewapi.SendVerificationEmailRequest,
+  newnewapi.EmptyResponse,
+  // TODO: the URL is temprorary!
+  `${BASE_URL_AUTH}/send_verification_email`,
+  'post',
+  payload,
+);
+
+/**
+ * Called from the Code Verification page.
+ * The arguments are the cached email address and the code
+ * manually input by the user.
+*/
+export const signInWithEmail = (
+  payload: newnewapi.EmailSignInRequest,
+) => fetchProtobuf<newnewapi.EmailSignInRequest, newnewapi.SignInResponse>(
+  newnewapi.EmailSignInRequest,
+  newnewapi.SignInResponse,
+  // TODO: the URL is temprorary!
+  `${BASE_URL_AUTH}/sign_in_with_email`,
+  'post',
+  payload,
+);
+
+// One-click sign up
+/*
+These functions are likely to be called client-side or in the on-page
+`getServerSideProps` function after the redirect from the corresponding
+providers (Apple, Facebook, etc.)
+*/
+
+export const signInWithApple = (
+  payload: newnewapi.AppleSignInRequest,
+) => fetchProtobuf<newnewapi.AppleSignInRequest, newnewapi.SignInResponse>(
+  newnewapi.AppleSignInRequest,
+  newnewapi.SignInResponse,
+  `${BASE_URL_AUTH}/sign_in_with_apple`,
+  'post',
+  payload,
+);
+
+export const signInWithGoogle = (
+  payload: newnewapi.GoogleSignInRequest,
+) => fetchProtobuf<newnewapi.GoogleSignInRequest, newnewapi.SignInResponse>(
+  newnewapi.GoogleSignInRequest,
+  newnewapi.SignInResponse,
+  `${BASE_URL_AUTH}/sign_in_with_google`,
+  'post',
+  payload,
+);
