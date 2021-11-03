98ec195b51b4f63243b86b82045a5e5380495612
uafrontender <mikhail.karkachov@gmail.com>
2021-11-03 18:22:26 +0300

    Update font imports

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,7 +33,29 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html translate="no">
-        <Head />
+        <Head>
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-Thin.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-ThinItalic.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-UltraLight.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-UltraLightItalic.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-Light.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-LightItalic.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-Regular.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-RegularItalic.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-Medium.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-MediumItalic.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-SemiBold.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-SemiBoldItalic.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-Bold.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-BoldItalic.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-ExtraBold.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-ExtraBoldItalic.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-Black.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-BlackItalic.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-Heavy.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Radomir Tinkov - Gilroy-HeavyItalic.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+          <link href="/fonts/font-styles.css" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <NextScript />
