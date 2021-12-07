3b079a7c4059893e9bc547e4183c268d235884c3
uafrontender <mikhail.karkachov@gmail.com>
2021-12-07 19:36:25 +0200

    Fix Lottie animation dependency
    Add Post creation API connection
    
    Add Post creation redux slice

--- pages/creation/[tab].tsx
+++ pages/creation/[tab].tsx
@@ -1,6 +1,12 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, {
+  useMemo,
+  useState,
+  useEffect,
+  useCallback,
+} from 'react';
 import Head from 'next/head';
 import styled from 'styled-components';
+import _compact from 'lodash/compact';
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
 import { NextPageContext } from 'next';
@@ -9,17 +15,23 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Input from '../../components/atoms/creation/Input';
 import Button from '../../components/atoms/Button';
 import FileUpload from '../../components/molecules/creation/FileUpload';
+import MobileField from '../../components/molecules/creation/MobileField';
 import Tabs, { Tab } from '../../components/molecules/Tabs';
 import CreationLayout from '../../components/templates/CreationLayout';
 
 import { NextPageWithLayout } from '../_app';
-import { useAppSelector } from '../../redux-store/store';
+import { useAppDispatch, useAppSelector } from '../../redux-store/store';
+import { setCreationTitle, setCreationMinBid, setCreationComments } from '../../redux-store/slices/creationStateSlice';
 
 export const CreationSecondStep = () => {
   const [minHeight, setMinHeight] = useState('100vh');
   const { t } = useTranslation('creation');
   const router = useRouter();
-  const tabs: Tab[] = [
+  const dispatch = useAppDispatch();
+  const { post, auction } = useAppSelector((state) => state.creation);
+  const { resizeMode } = useAppSelector((state) => state.ui);
+
+  const tabs: Tab[] = useMemo(() => [
     {
       nameToken: 'auction',
       url: '/creation/auction',
@@ -32,12 +44,67 @@ export const CreationSecondStep = () => {
       nameToken: 'crowdfunding',
       url: '/creation/crowdfunding',
     },
-  ];
+  ], []);
   const activeTabIndex = tabs.findIndex((tab) => tab.nameToken === router?.query?.tab);
-  const { resizeMode } = useAppSelector((state) => state.ui);
   const isMobile = ['mobile', 'mobileS', 'mobileM', 'mobileL'].includes(resizeMode);
+  const mobileFields = useMemo(() => _compact([
+    router?.query?.tab === 'multiple-choice' && {
+      key: 'options',
+    },
+    {
+      key: 'minimalBid',
+      value: '$1.00',
+    },
+    router?.query?.tab === 'crowdfunding' && {
+      key: 'min-backers',
+      value: '0',
+    },
+    {
+      key: 'expire',
+      value: 'Sat, 13 Nov 2021 at 2 PM',
+    },
+    {
+      key: 'post',
+      value: 'Right away',
+    },
+    {
+      key: 'comments',
+      type: 'toggle',
+      value: true,
+    },
+  ]), [router?.query?.tab]);
 
-  const handleSubmit = useCallback(() => {}, []);
+  const handleSubmit = useCallback(() => {
+  }, []);
+  const handleItemChange = useCallback((key: string, value: string | boolean) => {
+    if (key === 'title') {
+      dispatch(setCreationTitle(value));
+    } else if (key === 'minimalBid') {
+      dispatch(setCreationMinBid(value));
+    } else if (key === 'comments') {
+      dispatch(setCreationComments(value));
+    }
+  }, [dispatch]);
+  const renderField = useCallback((item) => {
+    let value: any = '';
+
+    if (item.key === 'minimalBid') {
+      value = `$${auction.minimalBid}`;
+    } else if (item.key === 'comments') {
+      value = post.options.commentsEnabled;
+    }
+
+    return (
+      <SFieldWrapper key={item.key}>
+        <MobileField
+          item={item}
+          value={value}
+          onChange={handleItemChange}
+        />
+        <SSeparator />
+      </SFieldWrapper>
+    );
+  }, [auction.minimalBid, handleItemChange, post.options.commentsEnabled]);
 
   useEffect(() => {
     setMinHeight(`${window.innerHeight}px`);
@@ -69,12 +136,16 @@ export const CreationSecondStep = () => {
               <>
                 <SItemWrapper>
                   <Input
+                    id="title"
+                    value={post?.title}
+                    onChange={handleItemChange}
                     placeholder={t('secondStep.input.placeholder')}
                   />
                 </SItemWrapper>
                 <SSeparator />
               </>
             )}
+            {mobileFields.map(renderField)}
           </SLeftPart>
         </SContent>
       </div>
@@ -186,7 +257,7 @@ const SItemWrapper = styled.div`
 const SSeparator = styled.div`
   width: 100%;
   border: 1px solid ${(props) => props.theme.colorsThemed.background.outlines1};
-  margin: 12px 0;
+  margin: 16px 0;
 `;
 
 const SButton = styled(Button)`
@@ -194,3 +265,5 @@ const SButton = styled(Button)`
   padding: 16px 20px;
   margin-bottom: 24px;
 `;
+
+const SFieldWrapper = styled.div``;
