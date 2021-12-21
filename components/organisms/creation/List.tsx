a27998428104ca327610e92a16699dff2b630320
Andrii Uhryn <ugrin14081994@gmail.com>
2021-12-21 15:16:10 +0200

    Fix First Step UI

--- components/organisms/creation/List.tsx
+++ components/organisms/creation/List.tsx
@@ -28,37 +28,27 @@ export const List: React.FC<IList> = (props) => {
 export default List;
 
 const SList = styled.div`
+  gap: 12px;
   display: flex;
-  margin-top: 34px;
+  margin-top: 40px;
   align-items: center;
   flex-direction: column;
   justify-content: center;
 
   ${(props) => props.theme.media.tablet} {
-    left: -8px;
-    width: calc(100% + 16px);
+    gap: 16px;
+    margin: 140px auto 0 auto;
     position: relative;
     max-width: 768px;
-    margin-top: 140px;
     flex-direction: row;
   }
 
   ${(props) => props.theme.media.laptop} {
-    left: unset;
-    margin: 164px auto 0 auto;
-    flex-direction: row;
+    gap: 32px;
+    margin: 152px auto 0 auto;
   }
 `;
 
 const SItemWrapper = styled.div`
   width: 100%;
-  margin: 6px 0;
-
-  ${(props) => props.theme.media.tablet} {
-    margin: 0 8px;
-  }
-
-  ${(props) => props.theme.media.tablet} {
-    margin: 0 16px;
-  }
 `;
