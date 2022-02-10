4c5eb63018ad4795428ac4f8bf6dcfd1d86eb31a
Mikhail Karkachov <1360242+mkarkachov@users.noreply.github.com>
2022-02-10 14:29:19 +0200

    Created chat api endpoints

---
+++ api/endpoints/chat.ts
@@ -0,0 +1,58 @@
+import { newnewapi } from 'newnew-api';
+import { BASE_URL, fetchProtobufProtectedIntercepted } from '../apiConfigs';
+
+export const BASE_URL_CHAT = `${BASE_URL}/chat`;
+
+export const createRoom = (payload: newnewapi.CreateRoomRequest) =>
+  fetchProtobufProtectedIntercepted<newnewapi.CreateRoomRequest, newnewapi.CreateRoomResponse>(
+    newnewapi.CreateRoomRequest,
+    newnewapi.CreateRoomResponse,
+    `${BASE_URL_CHAT}/create_room`,
+    'post',
+    payload
+  );
+
+export const deleteMyRoom = (payload: newnewapi.DeleteMyRoomRequest) =>
+  fetchProtobufProtectedIntercepted<newnewapi.DeleteMyRoomRequest, newnewapi.EmptyResponse>(
+    newnewapi.DeleteMyRoomRequest,
+    newnewapi.EmptyResponse,
+    `${BASE_URL_CHAT}/delete_room`,
+    'post',
+    payload
+  );
+
+export const getMessageCount = (payload: newnewapi.GetMessageCountRequest) =>
+  fetchProtobufProtectedIntercepted<newnewapi.GetMessageCountRequest, newnewapi.GetMessageCountResponse>(
+    newnewapi.GetMessageCountRequest,
+    newnewapi.GetMessageCountResponse,
+    `${BASE_URL_CHAT}/get_message_count`,
+    'post',
+    payload
+  );
+
+export const getMessages = (payload: newnewapi.GetMessagesRequest) =>
+  fetchProtobufProtectedIntercepted<newnewapi.GetMessagesRequest, newnewapi.GetMessagesResponse>(
+    newnewapi.GetMessagesRequest,
+    newnewapi.GetMessagesResponse,
+    `${BASE_URL_CHAT}/get_messages`,
+    'post',
+    payload
+  );
+
+export const getMyRooms = (payload: newnewapi.GetMyRoomsRequest) =>
+  fetchProtobufProtectedIntercepted<newnewapi.GetMyRoomsRequest, newnewapi.GetMyRoomsResponse>(
+    newnewapi.GetMyRoomsRequest,
+    newnewapi.GetMyRoomsResponse,
+    `${BASE_URL_CHAT}/get_my_rooms`,
+    'post',
+    payload
+  );
+
+export const sendMessage = (payload: newnewapi.SendMessageRequest) =>
+  fetchProtobufProtectedIntercepted<newnewapi.SendMessageRequest, newnewapi.SendMessageResponse>(
+    newnewapi.SendMessageRequest,
+    newnewapi.SendMessageResponse,
+    `${BASE_URL_CHAT}/send_message`,
+    'post',
+    payload
+  );
--- api/endpoints/subscription.ts
+++ api/endpoints/subscription.ts
@@ -1,62 +1,49 @@
 import { newnewapi } from 'newnew-api';
-import {
-  BASE_URL,
-  fetchProtobufProtectedIntercepted,
-} from '../apiConfigs';
+import { BASE_URL, fetchProtobufProtectedIntercepted } from '../apiConfigs';
 
 export const BASE_URL_SUBSCRIPTIONS = `${BASE_URL}/subscription`;
 
-export const subscribeToCreator = (
-  payload: newnewapi.SubscribeToCreatorRequest,
-) => fetchProtobufProtectedIntercepted<
-newnewapi.SubscribeToCreatorRequest, newnewapi.SubscribeToCreatorResponse>(
-  newnewapi.SubscribeToCreatorRequest,
-  newnewapi.SubscribeToCreatorResponse,
-  `${BASE_URL_SUBSCRIPTIONS}/subscribe_to_creator`,
-  'post',
-  payload,
-);
+export const subscribeToCreator = (payload: newnewapi.SubscribeToCreatorRequest) =>
+  fetchProtobufProtectedIntercepted<newnewapi.SubscribeToCreatorRequest, newnewapi.SubscribeToCreatorResponse>(
+    newnewapi.SubscribeToCreatorRequest,
+    newnewapi.SubscribeToCreatorResponse,
+    `${BASE_URL_SUBSCRIPTIONS}/subscribe_to_creator`,
+    'post',
+    payload
+  );
 
-export const unsubscribeFromCreator = (
-  payload: newnewapi.UnsubscribeFromCreatorRequest,
-) => fetchProtobufProtectedIntercepted<
-newnewapi.UnsubscribeFromCreatorRequest, newnewapi.UnsubscribeFromCreatorResponse>(
-  newnewapi.UnsubscribeFromCreatorRequest,
-  newnewapi.UnsubscribeFromCreatorResponse,
-  `${BASE_URL_SUBSCRIPTIONS}/unsubscribe_from_creator`,
-  'post',
-  payload,
-);
+export const unsubscribeFromCreator = (payload: newnewapi.UnsubscribeFromCreatorRequest) =>
+  fetchProtobufProtectedIntercepted<newnewapi.UnsubscribeFromCreatorRequest, newnewapi.UnsubscribeFromCreatorResponse>(
+    newnewapi.UnsubscribeFromCreatorRequest,
+    newnewapi.UnsubscribeFromCreatorResponse,
+    `${BASE_URL_SUBSCRIPTIONS}/unsubscribe_from_creator`,
+    'post',
+    payload
+  );
 
-export const getSubscriptionStatus = (
-  payload: newnewapi.SubscriptionStatusRequest,
-) => fetchProtobufProtectedIntercepted<
-newnewapi.SubscriptionStatusRequest, newnewapi.SubscriptionStatusResponse>(
-  newnewapi.SubscriptionStatusRequest,
-  newnewapi.SubscriptionStatusResponse,
-  `${BASE_URL_SUBSCRIPTIONS}/get_subscription_status`,
-  'post',
-  payload,
-);
+export const getSubscriptionStatus = (payload: newnewapi.SubscriptionStatusRequest) =>
+  fetchProtobufProtectedIntercepted<newnewapi.SubscriptionStatusRequest, newnewapi.SubscriptionStatusResponse>(
+    newnewapi.SubscriptionStatusRequest,
+    newnewapi.SubscriptionStatusResponse,
+    `${BASE_URL_SUBSCRIPTIONS}/get_subscription_status`,
+    'post',
+    payload
+  );
 
-export const getMySubscribers = (
-  payload: newnewapi.GetMySubscribersRequest,
-) => fetchProtobufProtectedIntercepted<
-newnewapi.GetMySubscribersRequest, newnewapi.GetMySubscribersResponse>(
-  newnewapi.GetMySubscribersRequest,
-  newnewapi.GetMySubscribersResponse,
-  `${BASE_URL_SUBSCRIPTIONS}/get_my_subscribers`,
-  'post',
-  payload,
-);
+export const getMySubscribers = (payload: newnewapi.GetMySubscribersRequest) =>
+  fetchProtobufProtectedIntercepted<newnewapi.GetMySubscribersRequest, newnewapi.GetMySubscribersResponse>(
+    newnewapi.GetMySubscribersRequest,
+    newnewapi.GetMySubscribersResponse,
+    `${BASE_URL_SUBSCRIPTIONS}/get_my_subscribers`,
+    'post',
+    payload
+  );
 
-export const getCreatorsImSubscribedTo = (
-  payload: newnewapi.EmptyRequest,
-) => fetchProtobufProtectedIntercepted<
-newnewapi.EmptyRequest, newnewapi.GetCreatorsImSubscribedToResponse>(
-  newnewapi.EmptyRequest,
-  newnewapi.GetCreatorsImSubscribedToResponse,
-  `${BASE_URL_SUBSCRIPTIONS}/get_creators_im_subscribed_to`,
-  'post',
-  payload,
-);
+export const getCreatorsImSubscribedTo = (payload: newnewapi.EmptyRequest) =>
+  fetchProtobufProtectedIntercepted<newnewapi.EmptyRequest, newnewapi.GetCreatorsImSubscribedToResponse>(
+    newnewapi.EmptyRequest,
+    newnewapi.GetCreatorsImSubscribedToResponse,
+    `${BASE_URL_SUBSCRIPTIONS}/get_creators_im_subscribed_to`,
+    'post',
+    payload
+  );
