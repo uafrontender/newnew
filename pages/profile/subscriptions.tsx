898ca2fdc0398599e5c4eeaf50761baf7d765634
RazgrizX <vladislav1996.06@gmail.com>
2022-05-02 14:12:54 +0400

    Add no content element to subscriptions

--- pages/profile/subscriptions.tsx
+++ pages/profile/subscriptions.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import type { GetServerSidePropsContext, NextPage } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { newnewapi } from 'newnew-api';
+import { useRouter } from 'next/router';
 
 import { NextPageWithLayout } from '../_app';
 // import { TTokenCookie } from '../../api/apiConfigs';
@@ -13,6 +14,10 @@ import MyProfileLayout from '../../components/templates/MyProfileLayout';
 import PostsFilterSection from '../../components/molecules/profile/PostsFilterSection';
 import { useGetSubscriptions } from '../../contexts/subscriptionsContext';
 import CreatorsList from '../../components/organisms/search/CreatorsList';
+import NoSubscriptions from '../../public/images/profile/No-subscriptions.png';
+import VerificationPassedIcon from '../../public/images/svg/icons/filled/VerificationPassed.svg';
+import Button from '../../components/atoms/Button';
+import InlineSvg from '../../components/atoms/InlineSVG';
 
 interface IMyProfileSubscriptions {
   user: Omit<newnewapi.User, 'toJSON'>;
@@ -43,6 +48,7 @@ const MyProfileSubscriptions: NextPage<IMyProfileSubscriptions> = ({
 }) => {
   const { isCreatorsImSubscribedToLoading, creatorsImSubscribedTo } =
     useGetSubscriptions();
+  const router = useRouter();
   console.log(isCreatorsImSubscribedToLoading);
   console.log(creatorsImSubscribedTo);
   return (
@@ -57,7 +63,60 @@ const MyProfileSubscriptions: NextPage<IMyProfileSubscriptions> = ({
               subscribedTo
             />
           ) : (
-            'NO CREATORS IMAGE HERE'
+            <NoContentMessageContainer>
+              <NoContentGraphicsContainer>
+                <SImage src={NoSubscriptions.src} />
+              </NoContentGraphicsContainer>
+              <NoContentInfoContainer>
+                <NoContentTitle>
+                  You’re not subscribed to any creators
+                </NoContentTitle>
+                <NoContentDescription>
+                  By subscribing to creators you will be able to:
+                </NoContentDescription>
+                <NoContentInstruction>
+                  <NoContentInstructionRecord>
+                    <NoContentInstructionPoint>
+                      <InlineSvg
+                        svg={VerificationPassedIcon}
+                        width='16px'
+                        height='16px'
+                      />
+                    </NoContentInstructionPoint>
+                    Direct message you
+                  </NoContentInstructionRecord>
+                  <NoContentInstructionRecord>
+                    <NoContentInstructionPoint>
+                      <InlineSvg
+                        svg={VerificationPassedIcon}
+                        width='16px'
+                        height='16px'
+                      />
+                    </NoContentInstructionPoint>
+                    Get a free vote on your
+                  </NoContentInstructionRecord>
+                  <NoContentInstructionRecord>
+                    <NoContentInstructionPoint>
+                      <InlineSvg
+                        svg={VerificationPassedIcon}
+                        width='16px'
+                        height='16px'
+                      />
+                    </NoContentInstructionPoint>
+                    Superpolls Add custom suggestions to your Superpolls
+                  </NoContentInstructionRecord>
+                </NoContentInstruction>
+                <SButton
+                  withShadow
+                  view='primaryGrad'
+                  onClick={() => {
+                    router.push('/');
+                  }}
+                >
+                  Find creators
+                </SButton>
+              </NoContentInfoContainer>
+            </NoContentMessageContainer>
           )}
         </SCardsSection>
       </SMain>
@@ -126,4 +185,109 @@ const SCardsSection = styled.div`
   ${(props) => props.theme.media.tablet} {
     margin-right: -32px !important;
   }
+`;
+
+const NoContentMessageContainer = styled.div`
+  margin: 8px 16px 30px !important;
+  padding: 16px 24px 30px !important;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colorsThemed.background.secondary};
+  width: 100%;
+  border-radius: 16px;
+
+  ${({ theme }) => theme.media.tablet} {
+    flex-direction: row;
+    padding: 24px 32px 24px 16px !important;
+    margin: 56px auto 30px !important;
+    width: auto;
+  }
+`;
+
+const NoContentGraphicsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 24px;
+
+  ${({ theme }) => theme.media.tablet} {
+    width: auto;
+  }
+`;
+
+const NoContentInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  ${({ theme }) => theme.media.tablet} {
+    align-items: flex-start;
+    margin-left: 24px;
+  }
+`;
+
+const SImage = styled.img`
+  object-fit: contain;
+  height: 165px;
+  width: 165px;
+
+  ${({ theme }) => theme.media.laptop} {
+    width: 245px;
+    height: 245px;
+  }
+`;
+
+const NoContentTitle = styled.div`
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 28px;
+  color: ${({ theme }) => theme.colorsThemed.text.primary};
+  margin-bottom: 8px;
+  width: 100%;
+  text-align: start;
+
+  ${({ theme }) => theme.media.laptop} {
+    font-size: 24px;
+    line-height: 32px;
+  }
+`;
+
+const NoContentDescription = styled.div`
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 20px;
+  color: ${({ theme }) => theme.colorsThemed.text.secondary};
+  margin-bottom: 18px;
+  width: 100%;
+  text-align: start;
+
+  ${({ theme }) => theme.media.laptop} {
+    font-size: 16px;
+    line-height: 24px;
+  }
+`;
+
+const NoContentInstruction = styled.div`
+  margin-bottom: 12px;
+`;
+
+const NoContentInstructionPoint = styled.div`
+  flex-shrink: 0;
+  margin-right: 6px !important;
+`;
+
+const NoContentInstructionRecord = styled.div`
+  display: flex;
+  align-items: center;
+
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20px;
+  color: ${({ theme }) => theme.colorsThemed.text.primary};
+  margin-bottom: 12px;
+`;
+
+const SButton = styled(Button)`
+  width: fit-content;
 `;
