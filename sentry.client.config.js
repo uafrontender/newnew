f86d66dede477683f180e9f5c612323fbeb7f7a0
RazgrizX <vladislav1996.06@gmail.com>
2022-05-30 13:57:59 +0400

    Report console errors to sentry as well

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -3,6 +3,7 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs';
+import { CaptureConsole } from '@sentry/integrations';
 
 const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
@@ -11,7 +12,14 @@ Sentry.init({
     SENTRY_DSN ||
     'https://88013d79fd0f45f0b219c26f458fa84b@o1263657.ingest.sentry.io/6453893',
   // Adjust this value in production, or use tracesSampler for greater control
+  integrations: [
+    new CaptureConsole({
+      levels: ['error'],
+    }),
+  ],
   tracesSampleRate: 1.0,
+  environment: 'prod',
+  maxBreadcrumbs: 50,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
