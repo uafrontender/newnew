ffefcd3f504c637d1c088fffab6c8a00fee79ce2
uafrontender <mikhail.karkachov@gmail.com>
2022-05-31 12:46:11 +0400

    Fix activity page

--- pages/[username]/activity.tsx
+++ pages/[username]/activity.tsx
@@ -132,7 +132,7 @@ const UserPageActivity: NextPage<IUserPageActivity> = ({
       return;
     }
 
-    if (!user.options.isActivityPrivate) {
+    if (user.options.isActivityPrivate) {
       return;
     }
 
