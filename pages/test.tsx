de0357b286fbe526e14b1bd5fc21a63f38ea2d0a
uafrontender <mikhail.karkachov@gmail.com>
2022-06-01 01:38:06 +0200

    Revert "Updated title in unsubscribe modal, removed test.tsx page, add redirection to 404 if username was not found"
    This reverts commit 1389a27bee4de71e68fd9643d538fd003cccec7c.

---
+++ pages/test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import type { NextPage } from 'next';
+
+const Test: NextPage = () => (
+  <div>
+    <main>
+      <h1>I am a test page</h1>
+    </main>
+  </div>
+);
+
+export default Test;
