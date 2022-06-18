e083d87e63124d8afee1effb56d94b81f83ef737
RazgrizX <vladislav1996.06@gmail.com>
2022-06-18 04:12:04 +0400

    Add getChunks utility

--- utils/getChunks/getChunks.test.tsx
+++ utils/getChunks/getChunks.test.tsx
@@ -1,6 +1,118 @@
-describe('Home', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+import getChunks from './getChunks';
+
+describe('text', () => {
+  it('empty', () => {
+    const input = '';
+    const chunks = getChunks(input);
+
+    expect(chunks.length).toBe(0);
+  });
+
+  it('normal', () => {
+    const input = 'Random text.';
+    const chunks = getChunks(input);
+
+    expect(chunks.length).toBe(1);
+
+    expect(chunks[0].type === 'text');
+    expect(chunks[0].text === input);
+  });
+});
+
+describe('parses hashtags', () => {
+  it('ignores hash only', () => {
+    const input = '#';
+    const chunks = getChunks(input);
+
+    expect(chunks.length).toBe(1);
+
+    expect(chunks[0].type === 'text');
+    expect(chunks[0].text === input);
+  });
+
+  it('single', () => {
+    const input = '#hashtag';
+    const chunks = getChunks(input);
+
+    expect(chunks.length).toBe(1);
+
+    expect(chunks[0].type === 'hashtag');
+    expect(chunks[0].text === input);
+  });
+
+  it('with undescore', () => {
+    const input = '#another_hashtag';
+    const chunks = getChunks(input);
+
+    expect(chunks.length).toBe(1);
+
+    expect(chunks[0].type === 'hashtag');
+    expect(chunks[0].text === input);
+  });
+
+  it('with capital letter', () => {
+    const input = '#HaShTaG';
+    const chunks = getChunks(input);
+
+    expect(chunks.length).toBe(1);
+
+    expect(chunks[0].type === 'hashtag');
+    expect(chunks[0].text === input);
+  });
+
+  it('with number', () => {
+    const input = '#1812';
+    const chunks = getChunks(input);
+
+    expect(chunks.length).toBe(1);
+
+    expect(chunks[0].type === 'hashtag');
+    expect(chunks[0].text === input);
+  });
+
+  it('multiple', () => {
+    const input = '#hashtag #another_hashtag';
+    const chunks = getChunks(input);
+
+    expect(chunks.length).toBe(3);
+
+    expect(chunks[0].type === 'hashtag');
+    expect(chunks[0].text === '#hashtag');
+
+    expect(chunks[1].type === 'text');
+    expect(chunks[1].text === ' ');
+
+    expect(chunks[2].type === 'hashtag');
+    expect(chunks[2].text === '#another-hashtag');
+  });
+
+  it('merged', () => {
+    const input = '#hashtag#anotherhashtag';
+    const chunks = getChunks(input);
+
+    expect(chunks.length).toBe(2);
+
+    expect(chunks[0].type === 'hashtag');
+    expect(chunks[0].text === '#hashtag');
+
+    expect(chunks[1].type === 'text');
+    expect(chunks[1].text === '#anotherhashtag');
+  });
+
+  it('keeps spaces', () => {
+    const input = '  #hashtag  ';
+    const chunks = getChunks(input);
+
+    expect(chunks.length).toBe(3);
+
+    expect(chunks[0].type === 'text');
+    expect(chunks[0].text === '  ');
+
+    expect(chunks[1].type === 'hashtag');
+    expect(chunks[1].text === '#hashtag');
+
+    expect(chunks[1].type === 'text');
+    expect(chunks[1].text === '  ');
   });
 });
 
---
+++ utils/getChunks/getChunks.ts
@@ -0,0 +1,44 @@
+interface TextChunk {
+  type: 'text';
+  text: string;
+}
+
+interface HashtagChunk {
+  type: 'hashtag';
+  text: string;
+}
+
+type Chunk = TextChunk | HashtagChunk;
+
+function getChunks(text: string, ignoreStart: boolean): Chunk[] {
+  if (text.length < 1) {
+    return [];
+  }
+
+  const regexp = ignoreStart
+    ? /(\s)(#[a-zA-Z0-9_]+)/
+    : /(^|\s)(#[a-zA-Z0-9_]+)/;
+  const match = text.match(regexp);
+
+  const chunks: Chunk[] = [];
+
+  if (match) {
+    if (match.index && match.index > 0) {
+      const textBeforeMatch = text.slice(0, match.index) + match[1];
+      chunks.push({ type: 'text', text: textBeforeMatch });
+    } else if (match[1].length > 0) {
+      chunks.push({ type: 'text', text: match[1] });
+    }
+
+    chunks.push({ type: 'hashtag', text: match[2] });
+
+    const remainder = text.slice((match.index || 0) + match[0].length);
+    chunks.push(...getChunks(remainder, true));
+  } else {
+    chunks.push({ type: 'text', text: text });
+  }
+
+  return chunks;
+}
+
+export default (text: string) => getChunks(text, false);
