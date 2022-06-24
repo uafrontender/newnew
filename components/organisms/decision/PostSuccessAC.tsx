e374100c941c186cccbe7b152afb5fd77dd952b1
Dmitry Zhukov <dmitry@newnew.co>
2022-06-24 19:04:38 +0000

    Resolve "Dmitry -> updates & fixes"

--- components/organisms/decision/PostSuccessAC.tsx
+++ components/organisms/decision/PostSuccessAC.tsx
@@ -167,7 +167,7 @@ const PostSuccessAC: React.FunctionComponent<IPostSuccessAC> = React.memo(
             handleToggleMuted={() => handleToggleMutedMode()}
             handleSetResponseViewed={(newValue) => setResponseViewed(newValue)}
           />
-          <SActivitesContainer>
+          <SActivitesContainer dimmedBackground={openedMainSection === 'main'}>
             {openedMainSection === 'main' ? (
               <>
                 <DecisionEndedBox
@@ -350,10 +350,13 @@ const SWrapper = styled.div`
   }
 `;
 
-const SActivitesContainer = styled.div`
+const SActivitesContainer = styled.div<{
+  dimmedBackground: boolean;
+}>`
   grid-area: activities;
 
-  background-color: ${({ theme }) => theme.colorsThemed.background.secondary};
+  background-color: ${({ theme, dimmedBackground }) =>
+    dimmedBackground ? theme.colorsThemed.background.secondary : 'transparent'};
   overflow: hidden;
   border-radius: 16px;
 
