e24a703de3e712a506a9007feb9e1e4a8fda28ec
Dmitry <dmitry@newnew.co>
2022-10-21 16:24:09 +0300

    Dmitry: try out including credentials for sticky sessions

--- contexts/socketContext.tsx
+++ contexts/socketContext.tsx
@@ -26,7 +26,8 @@ const SocketContextProvider: React.FC<ISocketContextProvider> = ({
             },
           }
         : {}),
-      transports: ['websocket', 'polling'],
+      withCredentials: true,
+      // transports: ['websocket', 'polling'],
     });
     setSocket(() => socketConnected);
 
