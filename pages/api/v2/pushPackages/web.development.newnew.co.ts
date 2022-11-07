235138bfcc8cb0a8585a98b8613b9e6f1d81075c
luka dodelia <luka@lnh.ge>
2022-11-07 18:51:41 +0400

    Sending pushPackage.zip for safari 15

--- pages/api/v2/pushPackages/web.development.newnew.co.ts
+++ pages/api/v2/pushPackages/web.development.newnew.co.ts
@@ -5,41 +5,27 @@ import fs from 'fs';
 
 const handler = async (
   req: NextApiRequest,
-  res: NextApiResponse<any>
+  res: NextApiResponse<any>,
 ): Promise<any> => {
   if (req.method === 'POST') {
     try {
       const filePath = path.join(
         process.cwd(),
-        '/public/pushPackage1663261361.zip'
+        '/public/pushPackage.zip',
       );
-
-      console.log(filePath, 'filePath');
-
-      const stat = fs.statSync(filePath);
-
-      const fileName = 'pushPackage.zip';
-      const fileType = 'application/zip';
-
-      res.writeHead(200, {
-        'Content-Type': fileType,
-        'Content-Disposition': `attachment; filename=${fileName}`,
-        'Content-Length': stat.size,
-      });
 
       const readStream = fs.createReadStream(filePath);
 
-      await new Promise((resolve) => {
-        console.log('HERE');
-        readStream.pipe(res);
-        readStream.on('end', resolve);
-      });
+      return readStream.pipe(res);
     } catch (err) {
       console.log(err, 'err');
-      // return res.status(400).json((err as any).error);
+      return res.status(400)
+        .json((err as any).error);
     }
   }
-  // return res.status(400).send('Invalid method or body');
+
+  return res.status(400)
+    .send('Invalid method or body');
 };
 
 export default handler;
