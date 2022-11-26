8cb07bf80194d88ba890a1eeda8482fc77109dc1
uafrontender <mikhail.karkachov@gmail.com>
2022-11-26 12:48:25 +0100

    New ChatMessage component

--- components/interfaces/ichat.ts
+++ components/interfaces/ichat.ts
@@ -2,7 +2,6 @@ import { newnewapi } from 'newnew-api';
 
 export interface IChatData {
   chatRoom: newnewapi.IChatRoom | null;
-  justSubscribed?: boolean;
   blockedUser?: boolean;
   isAnnouncement?: boolean;
   subscriptionExpired?: boolean;
