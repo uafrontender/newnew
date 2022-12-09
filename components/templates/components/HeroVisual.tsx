a75e45ceb28c3959ac94ff9834399c95d3ad78a5
RazgrizX <vladislav1996.06@gmail.com>
2022-12-09 21:19:12 +0400

    Add apng to intro, fix hourglass

--- components/templates/components/HeroVisual.tsx
+++ components/templates/components/HeroVisual.tsx
@@ -1,23 +1,21 @@
 /* eslint-disable consistent-return */
-import React, { useContext, useEffect, useMemo, useState } from 'react';
-import styled, { keyframes, useTheme } from 'styled-components';
+import React, { useContext, useEffect, useState } from 'react';
+import styled, { useTheme } from 'styled-components';
 import { motion } from 'framer-motion';
 import { AuthLayoutContext } from '../AuthLayout';
 import useImageLoaded from '../../../utils/hooks/useImageLoaded';
 import assets from '../../../constants/assets';
-// Cyclic dependency
-import isSafari from '../../../utils/isSafari';
 
 interface IHeroVisual {}
 
 const HeroVisual: React.FunctionComponent<IHeroVisual> = React.memo(() => {
   const theme = useTheme();
   const authLayoutContext = useContext(AuthLayoutContext);
-  const shouldUseCssFade = useMemo(() => isSafari(), []);
 
   const [currentState, setCurrentState] = useState<'intro' | 'hold' | 'outro'>(
-    shouldUseCssFade ? 'hold' : 'intro'
+    'intro'
   );
+
   const {
     ref: introRef,
     loaded: introLoaded,
@@ -41,48 +39,6 @@ const HeroVisual: React.FunctionComponent<IHeroVisual> = React.memo(() => {
     }
   }, [authLayoutContext.shouldHeroUnmount]);
 
-  if (shouldUseCssFade) {
-    return (
-      <SHeroVisual
-        exit={{
-          x: -1000,
-          y: 0,
-          opacity: 0,
-          transition: {
-            duration: 0.8,
-          },
-        }}
-      >
-        <SImageWrapperAnimated
-          style={{
-            opacity: currentState === 'hold' ? 1 : 0,
-          }}
-        >
-          <SImage
-            src={
-              theme.name === 'dark'
-                ? assets.signup.darkStatic
-                : assets.signup.lightStatic
-            }
-          />
-        </SImageWrapperAnimated>
-        <SImageWrapper
-          style={{
-            opacity: currentState === 'outro' ? 1 : 0,
-          }}
-        >
-          <SImage
-            src={
-              theme.name === 'dark'
-                ? assets.signup.darkOutroAnimated
-                : assets.signup.lightOutroAnimated
-            }
-          />
-        </SImageWrapper>
-      </SHeroVisual>
-    );
-  }
-
   return (
     <SHeroVisual
       exit={{
@@ -105,8 +61,8 @@ const HeroVisual: React.FunctionComponent<IHeroVisual> = React.memo(() => {
           }}
           src={
             theme.name === 'dark'
-              ? assets.signup.darkIntoAnimated
-              : assets.signup.lightIntoAnimated
+              ? assets.signup.darkIntoAnimated()
+              : assets.signup.lightIntoAnimated()
           }
           onLoad={onIntroLoaded}
         />
@@ -119,8 +75,8 @@ const HeroVisual: React.FunctionComponent<IHeroVisual> = React.memo(() => {
         <SImage
           src={
             theme.name === 'dark'
-              ? assets.signup.darkStatic
-              : assets.signup.lightStatic
+              ? assets.signup.darkIntroStatic
+              : assets.signup.lightIntroStatic
           }
         />
       </SImageWrapper>
@@ -132,8 +88,8 @@ const HeroVisual: React.FunctionComponent<IHeroVisual> = React.memo(() => {
         <SImage
           src={
             theme.name === 'dark'
-              ? assets.signup.darkOutroAnimated
-              : assets.signup.lightOutroAnimated
+              ? assets.signup.darkOutroAnimated()
+              : assets.signup.lightOutroAnimated()
           }
         />
       </SImageWrapper>
@@ -158,35 +114,6 @@ const SImageWrapper = styled.div`
 
   width: 400px;
   height: 600px;
-
-  ${({ theme }) => theme.media.laptop} {
-    width: 600px;
-    height: 700px;
-  }
-`;
-
-const FadeIn = keyframes`
-  0% {
-    transform: rotateX(30deg);
-    perspective: 1000px;
-    opacity: 0;
-  }
-  100% {
-    transform: rotateX(0deg);
-    perspective: 1000px;
-    opacity: 1;
-  }
-`;
-
-const SImageWrapperAnimated = styled.div`
-  position: absolute;
-  top: 0;
-  right: 0;
-
-  width: 400px;
-  height: 600px;
-
-  animation: ${FadeIn} 2s forwards linear;
 
   ${({ theme }) => theme.media.laptop} {
     width: 600px;
