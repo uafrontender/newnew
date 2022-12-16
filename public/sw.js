2c91fde165e15e4c777273ed1a3dda77535c4365
Valeria Mahilevich <valeria@newnew.co>
2022-12-16 16:15:28 +0100

    Don't show push notification when user currently is on direct messages page or on a page from web push target url

--- public/sw.js
+++ public/sw.js
@@ -6,16 +6,40 @@ self.addEventListener('push', (event) => {
   }
 
   event.waitUntil(
-    self.registration.showNotification(message.title, {
-      body: message.text,
-      badge: './favicon.png',
-      icon: './favicon.png',
-      tag: message.url,
-      renotify: true,
-      data: {
-        message,
-      },
-    })
+    clients
+      .matchAll({
+        type: 'window',
+      })
+      .then((clientList) => {
+        for (let i = 0; i < clientList.length; i += 1) {
+          const client = clientList[i];
+
+          // don't show pushes for dms is user is on dm page
+          if (
+            message?.url.includes('direct-message') &&
+            client.url.includes('direct-message') &&
+            client.focused
+          ) {
+            return;
+          }
+
+          // don't show pushes if user currently is on the same url as in push message
+          if (client.url.includes(message?.url) && client.focused) {
+            return;
+          }
+        }
+
+        self.registration.showNotification(message.title, {
+          body: message.text,
+          badge: './favicon.png',
+          icon: './favicon.png',
+          tag: message.url,
+          renotify: true,
+          data: {
+            message,
+          },
+        });
+      })
   );
 });
 
