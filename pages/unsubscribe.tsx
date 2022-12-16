418ff697bb950d9343302333858554fbd4d14a56
RazgrizX <vladislav1996.06@gmail.com>
2022-12-16 22:27:19 +0400

    Prepare UI for integration

--- pages/unsubscribe.tsx
+++ pages/unsubscribe.tsx
@@ -2,9 +2,9 @@ import React, { ReactElement, useContext, useEffect } from 'react';
 import Head from 'next/head';
 import { useTranslation } from 'next-i18next';
 import { motion } from 'framer-motion';
-import { useRouter } from 'next/router';
-import { GetStaticProps } from 'next';
+import { NextPageContext } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useEffectOnce } from 'react-use';
 
 import { NextPageWithLayout } from './_app';
 import AuthLayout, {
@@ -12,30 +12,19 @@ import AuthLayout, {
 } from '../components/templates/AuthLayout';
 import UnsubscribeMenu from '../components/organisms/UnsubscribeMenu';
 import { SUPPORTED_LANGUAGES } from '../constants/general';
-import { useAppSelector } from '../redux-store/store';
 
 const Unsubscribe = () => {
-  const router = useRouter();
   const { t } = useTranslation('page-Unsubscribe');
   const authLayoutContext = useContext(AuthLayoutContext);
-  const user = useAppSelector((state) => state.user);
 
   useEffect(() => {
     authLayoutContext.setShouldHeroUnmount(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  useEffect(() => {
-    if (!user.loggedIn && user._persist?.rehydrated) {
-      router.push(
-        `/sign-up?reason=comment&redirect=${encodeURIComponent(
-          window.location.href
-        )}`
-      );
-    } else {
-      // TODO: Send a request to unsubscribe. Once.
-    }
-  }, [user.loggedIn, user._persist?.rehydrated, router]);
+  useEffectOnce(() => {
+    // TODO: Send a request
+  });
 
   return (
     <>
@@ -71,17 +60,24 @@ const Unsubscribe = () => {
 
 export default Unsubscribe;
 
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getServerSideProps = async (context: NextPageContext) => {
   const translationContext = await serverSideTranslations(
     context.locale!!,
     ['page-Unsubscribe'],
     null,
     SUPPORTED_LANGUAGES
   );
+  // eslint-disable-next-line camelcase
+  const { token } = context.query;
 
   return {
     props: {
       ...translationContext,
+      ...(token
+        ? {
+            token,
+          }
+        : {}),
     },
   };
 };
