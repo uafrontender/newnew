f347434531ef78af64ea5e0f0e3d5c5740b46058
RazgrizX <vladislav1996.06@gmail.com>
2023-01-13 20:51:15 +0400

    Add verified icon to quick posts search

--- components/atoms/search/TopDecisionsResults.tsx
+++ components/atoms/search/TopDecisionsResults.tsx
@@ -9,7 +9,9 @@ import isBrowser from '../../../utils/isBrowser';
 import { Mixpanel } from '../../../utils/mixpanel';
 import secondsToDHMS from '../../../utils/secondsToDHMS';
 import UserAvatar from '../../molecules/UserAvatar';
+import InlineSvg from '../InlineSVG';
 import PostTitleContent from '../PostTitleContent';
+import VerificationCheckmark from '../../../public/images/svg/icons/filled/Verification.svg';
 
 interface IFunction {
   posts: newnewapi.IPost[];
@@ -72,6 +74,14 @@ const TopDecisionsResults: React.FC<IFunction> = ({ posts }) => {
                   )}
                   <SCreatorUsername>
                     {getDisplayname(data.creator)}
+                    {data.creator?.options?.isVerified && (
+                      <InlineSvg
+                        svg={VerificationCheckmark}
+                        width='20px'
+                        height='20px'
+                        fill='none'
+                      />
+                    )}
                   </SCreatorUsername>
                 </SPostData>
               </SLeftSide>
@@ -184,7 +194,10 @@ const SPostTitle = styled.span`
   pointer-events: none;
 `;
 
-const SCreatorUsername = styled.span`
+const SCreatorUsername = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
   color: ${({ theme }) => theme.colorsThemed.text.secondary};
 `;
 
