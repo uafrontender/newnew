239b46ffaf6857ad58710e1e3949bd4c5860b28b
Mikhail Karkachov <1360242+mkarkachov@users.noreply.github.com>
2023-01-19 17:15:06 +0100

    Chatlist fix

--- components/organisms/direct-messages/ChatContent.tsx
+++ components/organisms/direct-messages/ChatContent.tsx
@@ -337,7 +337,7 @@ const SBottomPart = styled.div`
   left: 0;
   right: 0;
   background: ${(props) => props.theme.colorsThemed.background.secondary};
-  padding: 10px 24px 24px;
+  padding: 10px 24px;
   ${(props) => props.theme.media.tablet} {
     position: absolute;
     padding: 20px;
