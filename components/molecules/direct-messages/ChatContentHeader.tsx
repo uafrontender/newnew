3c56e407601602bb7da856e74a6058142a5425e0
Vladislav Vishnevskii <vladislav@newnew.co>
2023-02-02 09:44:22 +0000

    Vlad/fixes 01 01

--- components/molecules/direct-messages/ChatContentHeader.tsx
+++ components/molecules/direct-messages/ChatContentHeader.tsx
@@ -85,7 +85,10 @@ const ChatContentHeader: React.FC<IFunctionProps> = ({
   }, []);
 
   const isDashboard = useMemo(() => {
-    if (router.asPath.includes('/creator/dashboard')) {
+    if (
+      router.asPath.includes('/creator/dashboard') ||
+      router.asPath.includes('/creator/bundles')
+    ) {
       return true;
     }
     return false;
@@ -95,7 +98,12 @@ const ChatContentHeader: React.FC<IFunctionProps> = ({
     setHiddenMessagesArea(true);
     if (isDashboard) {
       router.query = {};
-      router.push(`/creator/dashboard?tab=chat`);
+      if (router.asPath.includes('/creator/bundles')) {
+        router.push(`/creator/bundles?tab=chat`);
+      } else {
+        router.push(`/creator/dashboard?tab=chat`);
+      }
+
       setActiveChatRoom(null);
     } else if (isMobileOrTablet) {
       router.push(`/direct-messages`);
