3d9d0daf583f22eed76f25d0b11547c4e36d3e05
uafrontender <mikhail.karkachov@gmail.com>
2023-02-08 22:10:15 +0000

    Add min length for search

--- components/atoms/search/SearchInput.tsx
+++ components/atoms/search/SearchInput.tsx
@@ -207,10 +207,13 @@ const SearchInput: React.FC = React.memo(() => {
 
   useEffect(() => {
     const clearedSearchValue = getClearedSearchQuery(debouncedSearchValue);
-    if (clearedSearchValue) {
+    if (clearedSearchValue?.length > 1) {
       getQuickSearchResult(clearedSearchValue);
       setIsResultsDropVisible(true);
-    } else if (!clearedSearchValue && !isMobileOrTablet) {
+    } else if (
+      (!clearedSearchValue || clearedSearchValue.length === 1) &&
+      !isMobileOrTablet
+    ) {
       setIsResultsDropVisible(false);
       resetResults();
     }
