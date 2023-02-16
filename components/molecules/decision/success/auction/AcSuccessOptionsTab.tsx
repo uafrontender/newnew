ad9545320ce9cc43d1906d595b486f9a0fc316e5
Dmitry <dmitry@newnew.co>
2023-02-16 17:42:33 +0300

    Fix 3483

--- components/molecules/decision/success/auction/AcSuccessOptionsTab.tsx
+++ components/molecules/decision/success/auction/AcSuccessOptionsTab.tsx
@@ -18,6 +18,9 @@ import GradientMask from '../../../../atoms/GradientMask';
 import { Mixpanel } from '../../../../../utils/mixpanel';
 import getDisplayname from '../../../../../utils/getDisplayname';
 
+import Lottie from '../../../../atoms/Lottie';
+import loadingAnimation from '../../../../../public/animations/logo-loading-blue.json';
+
 interface IAcSuccessOptionsTab {
   post: newnewapi.Auction;
   handleGoBack: () => void;
@@ -38,6 +41,7 @@ const AcSuccessOptionsTab: React.FunctionComponent<IAcSuccessOptionsTab> = ({
   // Options
   const {
     processedOptions: options,
+    isLoading: optionsLoading,
     hasNextPage: hasNextOptionsPage,
     fetchNextPage: fetchNextOptionsPage,
   } = useAcOptions(
@@ -83,6 +87,19 @@ const AcSuccessOptionsTab: React.FunctionComponent<IAcSuccessOptionsTab> = ({
             active={showBottomGradient}
           />
         </>
+      ) : null}
+      {options.length === 0 && optionsLoading ? (
+        <SNoOptionsYet>
+          <Lottie
+            width={64}
+            height={64}
+            options={{
+              loop: true,
+              autoplay: true,
+              animationData: loadingAnimation,
+            }}
+          />
+        </SNoOptionsYet>
       ) : null}
       <SBidsContainer
         ref={(el) => {
@@ -204,4 +221,23 @@ const SLoaderDiv = styled.div`
 const SLoadMoreBtn = styled(Button)`
   width: 100%;
   height: 56px;
+`;
+
+// No options yet
+const SNoOptionsYet = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  min-height: 300px;
+
+  ${({ theme }) => theme.media.tablet} {
+    position: absolute;
+  }
+
+  ${({ theme }) => theme.media.laptop} {
+    min-height: 400px;
+  }
 `;
