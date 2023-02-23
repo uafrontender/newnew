5989f0718262d80cc9e82cf374fce8fb6b7cd5d7
Valeria Mahilevich <valeria@newnew.co>
2023-02-23 17:38:41 +0100

    Improve AnimationBackground animation

--- components/atoms/AnimationBackground.tsx
+++ components/atoms/AnimationBackground.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/require-default-props */
-import React, { useMemo, useState, useEffect } from 'react';
+import React, { useMemo, useState, useEffect, useRef } from 'react';
 import styled, { keyframes } from 'styled-components';
+import isBrowser from '../../utils/isBrowser';
 
 const AnimatedBackground: React.FunctionComponent<{
   className?: string;
@@ -17,102 +18,121 @@ const AnimatedBackground: React.FunctionComponent<{
     setTimeout(() => setDelayed(false), DELAY);
   }, []);
 
-  if (delayed) {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const [containerHeight, setContainerHeight] = useState<number>(0);
+
+  useEffect(() => {
+    if (isBrowser()) {
+      setContainerHeight(containerRef.current?.clientHeight || 0);
+    }
+  }, [delayed]);
+
+  if (delayed || !isBrowser()) {
     return null;
   }
 
   return (
-    <SContainer className={className}>
-      {elements.map((el, i) => (
-        <FloatingAsset
-          key={el + 1}
-          index={i}
-          src={src}
-          alt={alt}
-          delay={0}
-          top={-200 - i * 60}
-          {...(i % 2 === 0
-            ? {
-                left: i * 100,
-              }
-            : {
-                right: 50 + i * 50,
-              })}
-          noBlur={noBlur}
-        />
-      ))}
-      {elements.map((el, i) => (
-        <FloatingAsset
-          key={el + 2}
-          index={i}
-          src={src}
-          alt={alt}
-          delay={2}
-          top={-200 - i * 50}
-          {...(i % 2 !== 0
-            ? {
-                left: 50 + i * 200,
-              }
-            : {
-                right: 100 + i * 200,
-              })}
-          noBlur={noBlur}
-        />
-      ))}
-      {elements.map((el, i) => (
-        <FloatingAsset
-          key={el + 3}
-          index={i}
-          src={src}
-          alt={alt}
-          delay={3}
-          top={-200 - i * 50}
-          {...(i % 2 === 0
-            ? {
-                left: 100 + i * 100,
-              }
-            : {
-                right: 50 + i * 200,
-              })}
-          noBlur={noBlur}
-        />
-      ))}
-      {elements.map((el, i) => (
-        <FloatingAsset
-          key={el + 4}
-          index={i}
-          src={src}
-          alt={alt}
-          delay={5}
-          top={-200 - i * 50}
-          {...(i % 2 !== 0
-            ? {
-                left: 50 + i * 200,
-              }
-            : {
-                right: 100 + i * 100,
-              })}
-          noBlur={noBlur}
-        />
-      ))}
-      {elements.map((el, i) => (
-        <FloatingAsset
-          key={el + 5}
-          index={i}
-          src={src}
-          alt={alt}
-          delay={8}
-          top={-200 - i * 50}
-          {...(i % 2 === 0
-            ? {
-                left: 100 + i * 100,
-              }
-            : {
-                right: 100 + i * 200,
-              })}
-          noBlur={noBlur}
-        />
-      ))}
+    <SContainer className={className} ref={containerRef}>
+      {containerHeight && (
+        <>
+          {elements.map((el, i) => (
+            <FloatingAsset
+              key={`1-${el}`}
+              index={i}
+              src={src}
+              alt={alt}
+              delay={0}
+              top={-200 - i * 60}
+              transformHeight={containerHeight}
+              {...(i % 2 === 0
+                ? {
+                    left: i * 100,
+                  }
+                : {
+                    right: 50 + i * 50,
+                  })}
+              noBlur={noBlur}
+            />
+          ))}
+          {elements.map((el, i) => (
+            <FloatingAsset
+              key={`2-${el}`}
+              index={i}
+              src={src}
+              alt={alt}
+              delay={2}
+              top={-200 - i * 50}
+              transformHeight={containerHeight}
+              {...(i % 2 !== 0
+                ? {
+                    left: 50 + i * 200,
+                  }
+                : {
+                    right: 100 + i * 200,
+                  })}
+              noBlur={noBlur}
+            />
+          ))}
+          {elements.map((el, i) => (
+            <FloatingAsset
+              key={`3-${el}`}
+              index={i}
+              src={src}
+              alt={alt}
+              delay={3}
+              top={-200 - i * 50}
+              transformHeight={containerHeight}
+              {...(i % 2 === 0
+                ? {
+                    left: 100 + i * 100,
+                  }
+                : {
+                    right: 50 + i * 200,
+                  })}
+              noBlur={noBlur}
+            />
+          ))}
+          {elements.map((el, i) => (
+            <FloatingAsset
+              key={`4-${el}`}
+              index={i}
+              src={src}
+              alt={alt}
+              delay={5}
+              top={-200 - i * 50}
+              transformHeight={containerHeight}
+              {...(i % 2 !== 0
+                ? {
+                    left: 50 + i * 200,
+                  }
+                : {
+                    right: 100 + i * 100,
+                  })}
+              noBlur={noBlur}
+            />
+          ))}
+          {elements.map((el, i) => (
+            <FloatingAsset
+              key={`5-${el}`}
+              index={i}
+              src={src}
+              alt={alt}
+              delay={8}
+              top={-200 - i * 50}
+              transformHeight={containerHeight}
+              {...(i % 2 === 0
+                ? {
+                    left: 100 + i * 100,
+                  }
+                : {
+                    right: 100 + i * 200,
+                  })}
+              noBlur={noBlur}
+            />
+          ))}
+        </>
+      )}
     </SContainer>
   );
 });
@@ -121,7 +141,6 @@ export default AnimatedBackground;
 
 const SContainer = styled.div`
   position: absolute;
-  /* background-color: blue; */
   width: 100vw;
   height: 100%;
 `;
@@ -135,6 +154,7 @@ interface IFloatingAsset {
   right?: number;
   delay: number;
   noBlur?: boolean;
+  transformHeight: number;
 }
 
 const FloatingAsset: React.FunctionComponent<IFloatingAsset> = ({
@@ -146,6 +166,7 @@ const FloatingAsset: React.FunctionComponent<IFloatingAsset> = ({
   right,
   delay,
   noBlur,
+  transformHeight,
 }) => {
   // const width = useMemo(() => Math.random() * 170, []);
   const width = useMemo(() => Math.random() * 170, []);
@@ -156,10 +177,11 @@ const FloatingAsset: React.FunctionComponent<IFloatingAsset> = ({
       index={index}
       delay={delay}
       speed={speed}
-      transform={`scale(${index % 2 === 0 ? -1 : 1}, 1)`}
+      $transform={`scale(${index % 2 === 0 ? -1 : 1}, 1)`}
+      transformOffset={top}
+      transformHeight={transformHeight}
       style={{
         width,
-        top,
         ...(left
           ? {
               left,
@@ -175,10 +197,13 @@ const FloatingAsset: React.FunctionComponent<IFloatingAsset> = ({
   );
 };
 
-const RainingAnimation = (transform: string) => keyframes`
+const RainingAnimation = (
+  transform: string,
+  transformHeight: number
+) => keyframes`
   0% {
     opacity:0;
-    transform: translateY(-300px) ${transform};// translateY(-100px);
+    transform: ${transform} translateY(-300px);// translateY(-100px);
   }
   10% {
     opacity:1;
@@ -188,22 +213,29 @@ const RainingAnimation = (transform: string) => keyframes`
   }
   100% {
     opacity:0;
-    transform:  translateY(100vh)  ${transform};// translateY(140vh);
+    transform: ${transform} ${`translateY(${transformHeight}px)`};// translateY(140vh);
   }
 `;
 
 const SIcon = styled.div<{
   index: number;
   delay: number;
-  transform: string;
+  $transform: string;
   speed: number;
   noBlur?: boolean;
+  transformOffset: number;
+  transformHeight: number;
 }>`
   display: flex;
 
   position: absolute;
 
-  animation: ${({ transform }) => RainingAnimation(transform)} infinite;
+  transform: ${({ transformOffset, $transform }) =>
+    `${$transform} translateY(${transformOffset}px)`};
+
+  animation: ${({ $transform, transformHeight }) =>
+      RainingAnimation($transform, transformHeight)}
+    infinite;
   animation-timing-function: linear;
   animation-duration: ${({ speed }) => `${speed}s`};
   animation-delay: ${({ delay }) => `${delay}s`};
