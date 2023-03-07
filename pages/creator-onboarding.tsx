e6179f98e40da2a49cc44c84ccad29bc7f5238bf
RazgrizX <vladislav1996.06@gmail.com>
2023-03-08 01:07:44 +0400

    Redirect unauthenticated user from onboarding to sign up

--- pages/creator-onboarding.tsx
+++ pages/creator-onboarding.tsx
@@ -142,12 +142,22 @@ export const getServerSideProps: GetServerSideProps<
     'Cache-Control',
     'public, s-maxage=30, stale-while-revalidate=35'
   );
+  const { token } = context.query;
   const translationContext = await serverSideTranslations(
     context.locale!!,
     ['common', 'page-CreatorOnboarding'],
     null,
     SUPPORTED_LANGUAGES
   );
+
+  if (!token) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/sign-up?to=create',
+      },
+    };
+  }
 
   try {
     const countriesPayload = new newnewapi.EmptyRequest({});
