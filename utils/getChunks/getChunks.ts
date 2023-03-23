de0d24458144ead7aafba9f3e3f855321018649d
RazgrizX <vladislav1996.06@gmail.com>
2023-03-23 20:00:09 +0400

    Merge changes from dev

--- utils/getChunks/getChunks.ts
+++ utils/getChunks/getChunks.ts
@@ -6,14 +6,8 @@ function getChunks(text: string, ignoreStart: boolean): Chunk[] {
   }
 
   const regexp = ignoreStart
-<<<<<<< HEAD
-    ? /(\s)(#[^\s.,?!:;—\-\[\]{}\(\)"…\/\\|*^+~=%#$@]+)(\s|$)/
-    : /(^|\s)(#[^\s.,?!:;—\-\[\]{}\(\)"…\/\\|*^+~=%#$@]+)(\s|$)/;
-=======
-    ? /(\s)(#[a-zA-Z0-9_’']+)(\s|$)/
-    : /(^|\s)(#[a-zA-Z0-9_’']+)(\s|$)/;
-
->>>>>>> origin/development
+    ? /(\s)(#[^\s.,?:;—\-\[\]{}\(\)"…\/\\|*^+~=%#$@]+)(\s|$)/
+    : /(^|\s)(#[^\s.,?:;—\-\[\]{}\(\)"…\/\\|*^+~=%#$@]+)(\s|$)/;
   const match = text.match(regexp);
 
   const chunks: Chunk[] = [];
