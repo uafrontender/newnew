9b995bdcb613db7982de1d80fd580a94eb1d6a5d
uafrontender <mikhail.karkachov@gmail.com>
2023-04-20 16:53:16 +0000

    Resolve "Dmitry -> Implement SharePanel component"

---
+++ components/atoms/SharePanel.tsx
@@ -0,0 +1,207 @@
+import React, { useCallback, useState } from 'react';
+import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
+import { FacebookShareButton, TwitterShareButton } from 'react-share';
+
+import { Mixpanel } from '../../utils/mixpanel';
+import InlineSvg from './InlineSVG';
+import Caption from './Caption';
+
+import copyIcon from '../../public/images/svg/icons/outlined/Link.svg';
+import tiktokIcon from '../../public/images/svg/icons/socials/TikTok.svg';
+import twitterIcon from '../../public/images/svg/icons/socials/Twitter.svg';
+import facebookIcon from '../../public/images/svg/icons/socials/Facebook.svg';
+import instagramIcon from '../../public/images/svg/icons/socials/Instagram.svg';
+
+const SOCIAL_ICONS = {
+  copy: copyIcon,
+  tiktok: tiktokIcon,
+  twitter: twitterIcon,
+  facebook: facebookIcon,
+  instagram: instagramIcon,
+};
+
+interface ISharePanel {
+  linkToShare: string;
+  className?: string;
+  onCopyLink?: () => void;
+}
+
+const SharePanel: React.FunctionComponent<ISharePanel> = ({
+  className,
+  linkToShare,
+  onCopyLink,
+}) => {
+  const { t } = useTranslation('common');
+
+  const [isCopiedUrl, setIsCopiedUrl] = useState(false);
+
+  async function copyPostUrlToClipboard(url: string) {
+    if ('clipboard' in navigator) {
+      await navigator.clipboard.writeText(url);
+    } else {
+      document.execCommand('copy', true, url);
+    }
+  }
+
+  const handlerCopy = useCallback(() => {
+    if (window) {
+      Mixpanel.track('Copy Link', {
+        _stage: 'SharePanel',
+        _sharedUrl: linkToShare,
+      });
+      copyPostUrlToClipboard(linkToShare)
+        .then(() => {
+          setIsCopiedUrl(true);
+          setTimeout(() => {
+            setIsCopiedUrl(false);
+            onCopyLink?.();
+          }, 1000);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [linkToShare, onCopyLink]);
+
+  const trackShareClicked = useCallback(
+    (socialProvider: string) => {
+      if (window) {
+        Mixpanel.track(`Share to ${socialProvider} Clicked`, {
+          _stage: 'SharePanel',
+          _sharedUrl: linkToShare,
+        });
+      }
+    },
+    [linkToShare]
+  );
+
+  return (
+    <SWrapper className={className}>
+      <SItem key='facebook' itemType='facebook'>
+        <FacebookShareButton
+          url={linkToShare}
+          className='overridenReactShareButton'
+          resetButtonStyle={false}
+          onClickCapture={() => trackShareClicked('Facebook')}
+        >
+          <InlineSvg
+            svg={SOCIAL_ICONS.facebook as string}
+            width='50%'
+            height='50%'
+          />
+        </FacebookShareButton>
+        <SItemTitle variant={3} weight={600}>
+          {t(`socials.facebook`)}
+        </SItemTitle>
+      </SItem>
+      <SItem key='twitter' itemType='twitter'>
+        <TwitterShareButton
+          url={linkToShare}
+          className='overridenReactShareButton'
+          resetButtonStyle={false}
+          onClickCapture={() => trackShareClicked('Twitter')}
+        >
+          <InlineSvg
+            svg={SOCIAL_ICONS.twitter as string}
+            width='50%'
+            height='50%'
+          />
+        </TwitterShareButton>
+        <SItemTitle variant={3} weight={600}>
+          {t(`socials.twitter`)}
+        </SItemTitle>
+      </SItem>
+      <SItem key='copy' itemType='copy'>
+        <SItemButton type='copy' onClick={handlerCopy}>
+          <InlineSvg
+            svg={SOCIAL_ICONS.copy as string}
+            width='50%'
+            height='50%'
+          />
+        </SItemButton>
+        <SItemTitle variant={3} weight={600}>
+          {isCopiedUrl ? t(`socials.copied`) : t(`socials.copy`)}
+        </SItemTitle>
+      </SItem>
+    </SWrapper>
+  );
+};
+
+export default SharePanel;
+
+const SWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const SItem = styled.div<{
+  itemType: ISItemButton['type'];
+}>`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  .overridenReactShareButton {
+    width: 48px;
+    height: 48px;
+    display: flex;
+    overflow: hidden;
+    align-items: center;
+    border-radius: 16px;
+    border: none;
+    justify-content: center;
+    background: ${(props) =>
+      props.theme.colorsThemed.social[props.itemType].main};
+
+    cursor: pointer;
+  }
+`;
+
+interface ISItemButton {
+  type: 'facebook' | 'twitter' | 'instagram' | 'tiktok' | 'copy';
+}
+
+const SItemButton = styled.div<ISItemButton>`
+  width: 48px;
+  height: 48px;
+  display: flex;
+  overflow: hidden;
+  align-items: center;
+  border-radius: 16px;
+  justify-content: center;
+  background: ${(props) => props.theme.colorsThemed.social[props.type].main};
+
+  cursor: pointer;
+`;
+
+const SItemTitle = styled(Caption)`
+  color: ${(props) => props.theme.colorsThemed.text.tertiary};
+  margin-top: 6px;
+  white-space: pre;
+  min-width: 56px;
+  text-align: center;
+`;
+
+// Might be useful if one of the older designs gets re-established
+// const SItemButtonWide = styled.div<ISItemButton>`
+//   width: 100%;
+//   height: 36px;
+//   display: flex;
+//   overflow: hidden;
+//   align-items: center;
+//   justify-content: center;
+//   gap: 4px;
+//   border-radius: 12px;
+//   background: ${(props) => props.theme.colorsThemed.social[props.type].main};
+
+//   font-weight: bold;
+//   font-size: 14px;
+//   line-height: 24px;
+
+//   color: #ffffff;
+//   cursor: pointer;
+// `;
--- components/molecules/decision/common/PostShareEllipseModal.tsx
+++ components/molecules/decision/common/PostShareEllipseModal.tsx
@@ -1,28 +1,11 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'next-i18next';
 import styled from 'styled-components';
 
-import { Mixpanel } from '../../../../utils/mixpanel';
-
 import Headline from '../../../atoms/Headline';
-import { InlineSvg } from '../../../atoms/InlineSVG';
 import EllipseModal from '../../../atoms/EllipseModal';
 
-import copyIcon from '../../../../public/images/svg/icons/outlined/Link.svg';
-import tiktokIcon from '../../../../public/images/svg/icons/socials/TikTok.svg';
-import twitterIcon from '../../../../public/images/svg/icons/socials/Twitter.svg';
-import facebookIcon from '../../../../public/images/svg/icons/socials/Facebook.svg';
-import instagramIcon from '../../../../public/images/svg/icons/socials/Instagram.svg';
-
-const SOCIAL_ICONS: any = {
-  copy: copyIcon,
-  tiktok: tiktokIcon,
-  twitter: twitterIcon,
-  facebook: facebookIcon,
-  instagram: instagramIcon,
-};
+import SharePanel from '../../../atoms/SharePanel';
 
 interface IPostShareEllipseModal {
   isOpen: boolean;
@@ -36,91 +19,16 @@ const PostShareEllipseModal: React.FunctionComponent<IPostShareEllipseModal> =
   React.memo(({ isOpen, zIndex, postUuid, postShortId, onClose }) => {
     const { t } = useTranslation('common');
 
-    // const socialButtons = useMemo(() => [
-    //   {
-    //     key: 'twitter',
-    //   },
-    //   {
-    //     key: 'facebook',
-    //   },
-    //   {
-    //     key: 'instagram',
-    //   },
-    //   {
-    //     key: 'tiktok',
-    //   },
-    //   {
-    //     key: 'copy',
-    //   },
-    // ], []);
-    // const renderItem = (item: any) => (
-    //   <SItem key={item.key}>
-    //     <SItemButton type={item.key}>
-    //       <InlineSVG
-    //         svg={SOCIAL_ICONS[item.key] as string}
-    //         width="50%"
-    //         height="50%"
-    //       />
-    //     </SItemButton>
-    //     <SItemTitle variant={3} weight={600}>
-    //       {t(`socials.${item.key}`)}
-    //     </SItemTitle>
-    //   </SItem>
-    // );
-
-    const [isCopiedUrl, setIsCopiedUrl] = useState(false);
-
-    async function copyPostUrlToClipboard(url: string) {
-      if ('clipboard' in navigator) {
-        await navigator.clipboard.writeText(url);
-      } else {
-        document.execCommand('copy', true, url);
-      }
-    }
-
-    const handlerCopy = useCallback(() => {
-      if (window) {
-        const url = `${window.location.origin}/p/${postShortId || postUuid}`;
-        Mixpanel.track('Copy Link Post', {
-          _stage: 'Post',
-          _postUuid: postUuid,
-        });
-        copyPostUrlToClipboard(url)
-          .then(() => {
-            setIsCopiedUrl(true);
-            setTimeout(() => {
-              setIsCopiedUrl(false);
-              onClose();
-            }, 1000);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
-    }, [postShortId, postUuid, onClose]);
-
     return (
       <EllipseModal show={isOpen} zIndex={zIndex} onClose={onClose}>
         <ButtonsSection>
           <Headline variant={6}>{t('shareTo')}</Headline>
-          <SSocialsSection>
-            <SItem>
-              <SItemButtonWide
-                type='copy'
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handlerCopy();
-                }}
-              >
-                <InlineSvg
-                  svg={SOCIAL_ICONS.copy as string}
-                  width='24px'
-                  height='24px'
-                />
-                {isCopiedUrl ? t('ellipse.linkCopied') : t('ellipse.copyLink')}
-              </SItemButtonWide>
-            </SItem>
-          </SSocialsSection>
+          <SSharePanel
+            linkToShare={`${window.location.origin}/p/${
+              postShortId || postUuid
+            }`}
+            onCopyLink={onClose}
+          />
         </ButtonsSection>
       </EllipseModal>
     );
@@ -132,36 +40,6 @@ const ButtonsSection = styled.div`
   padding: 16px;
 `;
 
-const SSocialsSection = styled.div`
+const SSharePanel = styled(SharePanel)`
   padding: 16px;
-`;
-
-const SItem = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-`;
-
-interface ISItemButton {
-  type: 'facebook' | 'twitter' | 'instagram' | 'tiktok' | 'copy';
-}
-
-const SItemButtonWide = styled.div<ISItemButton>`
-  width: 100%;
-  height: 36px;
-  display: flex;
-  overflow: hidden;
-  align-items: center;
-  justify-content: center;
-  gap: 4px;
-  border-radius: 12px;
-  background: ${(props) => props.theme.colorsThemed.social[props.type].main};
-
-  font-weight: bold;
-  font-size: 14px;
-  line-height: 24px;
-
-  color: #ffffff;
-  cursor: pointer;
 `;
