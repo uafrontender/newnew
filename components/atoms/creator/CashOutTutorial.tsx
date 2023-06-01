0f30b6d8a63397b3d30ddb7d26f0966367f5a7f2
uafrontender <mikhail.karkachov@gmail.com>
2023-06-01 17:29:58 +0000

    Vlad/fixes 01 06

---
+++ components/atoms/creator/CashOutTutorial.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import styled, { useTheme } from 'styled-components';
+import Text from '../Text';
+import InlineSvg from '../InlineSVG';
+import infoIcon from '../../../public/images/svg/icons/filled/Info.svg';
+
+const CashOutTutorial: React.FC = () => {
+  const { t } = useTranslation('page-Creator');
+  const theme = useTheme();
+
+  /* const handleClose = useCallback(() => {
+    // TODO: what should we do on close? close forever?
+  }, []); */
+
+  return (
+    <SContainer>
+      <SImageWrapper>
+        <InlineSvg
+          svg={infoIcon}
+          fill={theme.colors.white}
+          width='20px'
+          height='20px'
+        />
+      </SImageWrapper>
+      <SStripeBlockText variant={2} weight={600}>
+        {t('dashboard.earnings.cashOutTutorial.gracePeriod')}
+      </SStripeBlockText>
+      {/* TODO: add close icon */}
+    </SContainer>
+  );
+};
+
+export default CashOutTutorial;
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  background: ${({ theme }) => theme.colorsThemed.accent.success};
+  border-radius: 16px;
+
+  padding: 16px;
+  margin-bottom: 16px;
+
+  ${(props) => props.theme.media.tablet} {
+    padding: 24px;
+    margin-bottom: 24px;
+  }
+`;
+
+const SImageWrapper = styled.div`
+  margin-right: 8px;
+`;
+
+const SStripeBlockText = styled(Text)`
+  color: #ffffff;
+`;
