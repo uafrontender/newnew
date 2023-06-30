4807409e8ee6de8af2b044d3119bece863d43a98
Valeria Mahilevich <valeria@newnew.co>
2023-06-30 17:25:36 +0200

    Preview

--- ignore-step.sh
+++ ignore-step.sh
@@ -2,7 +2,7 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "development" || "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "dmitry/attempt-to-enhance-language-change" || "$VERCEL_GIT_COMMIT_REF" == "vlad/fixes-25-05-23" ||  "$VERCEL_GIT_COMMIT_REF" == "valeria/search-redesign" || "$VERCEL_GIT_COMMIT_REF" == "dmitry/revamp-responses-slider-to-custom" || "$VERCEL_GIT_COMMIT_REF" == "valeria/chat" ]]; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "development" || "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "dmitry/attempt-to-enhance-language-change" || "$VERCEL_GIT_COMMIT_REF" == "vlad/fixes-25-05-23" ||  "$VERCEL_GIT_COMMIT_REF" == "valeria/safari-header-footer" || "$VERCEL_GIT_COMMIT_REF" == "dmitry/revamp-responses-slider-to-custom" ]]; then
   # Proceed with the build
   echo "✅ - Build can proceed"
   exit 1;
