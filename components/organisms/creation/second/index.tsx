c808d9b3df30102dcf33d1911b0ab14e70dba2e0
Vladislav <vladislav@newnew.co>
2023-07-05 16:12:25 +0400

    Clean up after test fixes

--- components/organisms/creation/second/index.tsx
+++ components/organisms/creation/second/index.tsx
@@ -585,9 +585,6 @@ export const CreationSecondStepContent: React.FC<
         xhrRef.current = undefined;
       } catch (error: any) {
         // TODO: Change this overcomplicated approach
-        // TODO: remove
-        console.log(error);
-        console.log(error.message);
         if (error.message === 'Upload failed') {
           setCreationFileUploadError(true);
           showErrorToastPredefined(undefined);
