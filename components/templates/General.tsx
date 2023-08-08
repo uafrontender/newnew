c3ad8de3c14543c01388d0a9fdc831e4187cb006
Valeria Mahilevich <valeria@newnew.co>
2023-08-08 13:00:14 +0200

    Animate transform instead of bottom for cookies

--- components/templates/General.tsx
+++ components/templates/General.tsx
@@ -365,12 +365,16 @@ interface ICookieContainer {
 }
 
 const CookieContainer = styled.div<ICookieContainer>`
-  left: 50%;
-  bottom: ${(props) => (props.bottomNavigationVisible ? 62 : 6)}px;
-  z-index: ${(props) => props.zIndex};
   position: fixed;
-  transform: translateX(-50%);
-  transition: bottom ease 0.5s;
+  left: 50%;
+  bottom: 6px;
+
+  transform: ${(props) =>
+    props.bottomNavigationVisible
+      ? `translate(-50%, -56px)`
+      : `translate(-50%, 0)`};
+  transition: transform ease 0.5s;
+  z-index: ${(props) => props.zIndex};
   pointer-events: none;
 
   ${(props) => props.theme.media.tablet} {
